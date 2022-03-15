f489218577dd448a1493cb100967b16e55599735
Edgar Chamorro Astudillo <edfabrizio05@gmail.com>
2022-03-15 03:22:43 -0500

    Api Actualizada Login

---
+++ apiFlutter/login.php
@@ -0,0 +1,18 @@
+<?php 
+	require_once("conexion.php");
+
+	$email = $_POST['Email'];
+	$password = $_POST['Password'];
+
+	$sql = ("SELECT * FROM usuario WHERE Email = '".$email."' AND Password = '".$password."'");
+
+	$result = mysqli_query($mysqli,$sql);
+	$count = mysqli_num_rows($result);
+
+	if ($count == 1) {
+		echo json_encode("Success");
+	}else{
+		echo json_encode("Error");
+	}
+
+?>
